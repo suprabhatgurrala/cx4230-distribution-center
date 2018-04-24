dd96628ea0386eb4b78749e33000d5371b18b2c6
Nick Angalet <nangalet@gmail.com>
2018-04-23 20:56:11 -0400

    Update transportvehicle.py

--- transportvehicle.py
+++ transportvehicle.py
@@ -1,68 +1,28 @@
 class TransportVehicle:
-    """                                                                                    
-        Object to represent a transport vehicle                                            
+    """                                                                                                                          
+    Object to represent a transport vehicle                                                                                      
+    """
+
+    def __init__(self, capacity):
+        """                                                                                                                      
+        Initialization function                                                                                                  
+        :param capacity: number of packages this vehicle can transport                                                           
         """
+        self.capacity = capacity
+        self.package_list = []
+        self.is_free = True
 
-    def __init__(self, capacity, numPackages, average_speed):
-        """                                                                                
-        Initialization function                                                            
-        :param type: the type of vehicle Delivery or Transport                             
-        :param capacity: number of packages this vehicle can transport                     
-        :param average_speed: average speed while transporting                             
+    def is_full(self):
+        if len(self.package_list) == self.capacity:
+            return True
+        else:
+            return False
+
+    def add_package(self, package):
+        """                                                                                                                      
+        param x: number of packages to add                                                                                       
         """
-        self.capacity = capacity; "measure of storage space in truck"
-        self.numPackages = numPackages; "number of packages in the truck"
-        self.average_speed = average_speed;
-        pass
-
-    def addPackage(x):
-        """                                                                                
-        param x: number of packages to add                                                 
-        """
-        self.numPackages += x;
-        pass
-
-
-    def depart_from_warehouse_handler():
-        """                                                                               \
-        Method to handle a depart from warehouse event                                    \
-                                                                                           
-        Scheduled elsewhere                                                                
-        :param params:                                                                    \
-        """
-        numTVehiclesAvailable--; "should be function call"
-        schedule(self.arrive_at_other_warehouse_handler(), currentTime()+270);
-        pass
-
-    def arrive_at_other_warehouse_handler():
-        """                                                                                
-        Method to handle an arrive at warehouse event                                      
-        :param params:                                                                     
-        """
-        self.numPackages = 0;
-        schedule(self.depart_from_other_warehouse_handler(), currentTime()+45);
-        pass
-
-    def depart_from_other_warehouse_handler():
-        """                                                                                
-        Method to handle an depart from warehouse event                                    
-        :param params:                                                                     
-        """
-        schedule(self.arrive_at_warehouse_handler(), currentTime()+270);
-        pass
-
-    def arrive_at_warehouse_handler():
-        """                                                                                
-        Method to handle an arrive at warehouse event                                     \
-        Need to: signal that vehicle is ready to be loaded                                \
-        global variable numTVehiclesAvailable: number of transport vehicles at warehouse  \
-                                                                                           
-        :param params:                                                                    \
-                                                                                           
-        """
-        numTVehiclesAvailable++; "function call"
-
-        pass
+        self.package_list.append(package)
 
 
 
